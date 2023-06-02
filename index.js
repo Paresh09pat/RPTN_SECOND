8061edab5ed4af3757cebb496aa21e3912c2fd43
Paresh <09patilparesh@gmail.com>
2023-06-02 11:44:33 +0530

    ok

--- index.js
+++ index.js
@@ -110,3 +110,16 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
   closeIcon.addEventListener('click', () => {
     inputBox.classList.remove('open');
   });
+
+
+// hide nav bar
+  var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("navbar").classList.remove("hidden");
+  } else {
+    document.getElementById("navbar").classList.add("hidden");
+  }
+  prevScrollPos = currentScrollPos;
+};
